0a9520ebe540d1b98f677242bec818b681aa8f98
Silvio <silvio0407@gmail.com>
2017-10-19 22:12:06 -0200

    Adicionando dados no banco de dados via POST.

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -14,6 +14,15 @@ this.getNoticia = function(client, callback){
 	client.query('select * from noticias where id_noticia = 1', callback);	
 };
 
+this.salvarNoticia = function(noticia, client, callback){
+
+	client.connect();
+
+	client.query('insert into noticias(titulo, noticia) values($1, $2)', 
+		[noticia.titulo, noticia.noticia], callback)
+
+}
+
 return this;
 
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,29 @@
 
-module.exports = function(app){
-	app.get('/formulario_inclusao_noticia', function(req, res){
+module.exports = function(application){
+	application.get('/formulario_inclusao_noticia', function(req, res){
 		res.render("admin/form_add_noticia")
 
 	});
 
-	app.post('/noticias/salvar', function(req, res){
-		var noticias = req.body;
-		res.send(noticias);
+	application.post('/noticias/salvar', function(req, res){
+		var noticia = req.body;
+
+		//conexao
+
+		//model
+
+		var client = application.config.dbConnection();
+
+		var noticiasModel = application.app.models.noticiasModel;
+
+		noticiasModel.salvarNoticia(noticia, client, function(error,result){
+
+			//res.send(result);
+
+			//res.render("noticias/noticias", {noticias : result.rows});
+			res.redirect('/noticias');
+
+		});
 
 	});
 };
